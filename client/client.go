3c6fa828a1b83371099d073bd886580d5d526884
mieubrisse <mieubrisse@gmail.com>
2020-08-22 18:41:24 -0300

    Non-compiling port from old Kurtosis

---
+++ client/client.go
@@ -0,0 +1,39 @@
+package client
+
+import (
+	"github.com/palantir/stacktrace"
+	"github.com/sirupsen/logrus"
+	"os"
+	"strings"
+)
+
+func Run(testNamesFilepath string, test string) error {
+	testNamesFilepath = strings.TrimSpace(testNamesFilepath)
+	test = strings.TrimSpace(test)
+
+	isTestNamesFilepathEmpty := len(testNamesFilepath) == 0
+	isTestEmpty := len(test) == 0
+
+	// Only one of these should be set; if both are set then it's an error
+	if isTestNamesFilepathEmpty == isTestEmpty {
+		return stacktrace.NewError("Exactly one of test-names-filepath and the test-name-to-run should be set")
+	}
+
+	if !isTestNamesFilepathEmpty {
+		fp, err := os.OpenFile(testNamesFilepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			logrus.Errorf("No file exists at %v", testNamesFilepath)
+			os.Exit(1)
+		}
+		defer fp.Close()
+
+		testNames := []string{
+			"test1",
+			"test2",
+			"test3",
+		}
+		for _, line := range testNames {
+			fp.WriteString(line + "\n")
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kurtosis-tech/kurtosis-go
 
 go 1.13
 
-require github.com/sirupsen/logrus v1.6.0 // indirect
+require (
+	github.com/palantir/stacktrace v0.0.0-20161112013806-78658fd2d177 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+)
