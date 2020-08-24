2d73a7864643cc9e2d3600133479f47421e00909
mieubrisse <mieubrisse@gmail.com>
2020-08-24 17:48:49 -0300

    Add example test

--- example_impl/testsuite/example_testsuite.go
+++ example_impl/testsuite/example_testsuite.go
@@ -4,11 +4,12 @@ import (
 	"github.com/kurtosis-tech/kurtosis-go/lib/testsuite"
 )
 
+// TODO example of parameterizing your image
 type ExampleTestsuite struct {}
 
 func (e ExampleTestsuite) GetTests() map[string]testsuite.Test {
 	return map[string]testsuite.Test{
-
+		"exampleTest": ExampleTest{},
 	}
 }
 
