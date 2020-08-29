a6bc613020e11601dc779609a5f90568d806c560
mieubrisse <mieubrisse@gmail.com>
2020-08-29 18:47:14 -0300

    Add copyright notice to test suite metadata class

--- lib/client/test_suite_metadata.go
+++ lib/client/test_suite_metadata.go
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2020 - present Kurtosis Technologies LLC.
+ * All Rights Reserved.
+ */
+
 package client
 
 // Package class, intended to get written to JSON, containing metadata about the test suite
