b9b2fdc5ca5871e4c8de6507c6165aec5ba1f3d3
mieubrisse <mieubrisse@gmail.com>
2020-10-12 22:10:54 +0400

    Bugfixes with bootstrap.sh

--- bootstrap/README.md
+++ bootstrap/README.md
@@ -2,8 +2,10 @@ My Kurtosis Testsuite
 =====================
 Welcome to your new Kurtosis testsuite skeleton! To get started:
 
-1. Modify the files within the `example_impl` as you see fit
-2. Run `scripts/build_and_run.sh all` to run your test suite with the latest version of Kurtosis
+1. Run `git init` to initialize this directory as a Git repo
+1. Run `go get github.com/kurtosis-tech/kurtosis-go` to pull the Go client lib
+1. Using the documentation on [the Kurtosis docs](https://github.com/kurtosis-tech/kurtosis-docs), modify the files within the `example_impl` in order to build a testsuite for your use case
+1. Run `scripts/build_and_run.sh all` to build and run your test suite with the latest version of Kurtosis
 
 Some helpful tips:
 * `build_and_run.sh` can be run with the `build` argument to just build, or `run` to just run
--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,5 +1,8 @@
 # Deletes all the extraneous files, leaving a repo containing only the example impl and necessary infrastructure needed to write a testsuite
 
+EXAMPLE_IMPL_DIRNAME="example_impl"
+README_FILENAME="README.md"
+
 set -euo pipefail
 script_dirpath="$(cd "$(dirname "${0}")" && pwd)"
 root_dirpath="$(dirname "${script_dirpath}")"
@@ -8,9 +11,11 @@ read -p "VERIFICATION: This will delete nearly all files in ${root_dirpath}, lea
 
 read -p "FINAL VERIFICATION: you DO want to delete files like the .git dir to bootstrap a new testsuite, correct? (Ctrl-C to abort, ENTER to continue)"
 
+read -p "New Go module name (e.g. github.com/my-org/my-repo): " new_module_name
+
 find "${root_dirpath}" \
     ! -name bootstrap \
-    ! -name example_impl \
+    ! -name "${EXAMPLE_IMPL_DIRNAME}" \
     ! -name go.mod \
     ! -name go.sum \
     ! -name scripts \
@@ -19,5 +24,12 @@ find "${root_dirpath}" \
     -exec rm -rf {} \;
 
 cp "${script_dirpath}/README.md" "${root_dirpath}/"
+
+# Replace module names
+existing_module_name="$(grep "module" "${root_dirpath}/go.mod" | awk "${print $2}")"
+sed -i'' "s,${existing_module_name},${new_module_name},g" go.mod
+# We search for old_module_name/example_impl because we don't want the old_module_name/lib entries to get renamed
+sed -i'' "s,${existing_module_name}/${EXAMPLE_IMPL_DIRNAME},${new_module_name}/${EXAMPLE_IMPL_DIRNAME},g" $(find "${root_dirpath}" -type f)
+
 rm -rf "${script_dirpath}"
 echo "Bootstrap complete; view the README.md in ${root_dirpath} for next steps"
