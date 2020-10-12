ca0b7a7734638fa121063a3cdcd15aea68563ffd
mieubrisse <mieubrisse@gmail.com>
2020-10-12 21:45:22 +0400

    Bugfix in bootstrap.sh

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,14 +3,13 @@
 set -euo pipefail
 script_dirpath="$(cd "$(dirname "${0}")" && pwd)"
 root_dirpath="$(dirname "${script_dirpath}")"
-script_dirname="$(dirname "${script_dirpath}")"
 
 read -p "VERIFICATION: This will delete nearly all files in ${root_dirpath}, leaving only what's necessary for writing a new Kurtosis Go testsuite! Are you sure you want to proceed? (Ctrl-C to abort, ENTER to continue)"
 
 read -p "FINAL VERIFICATION: you DO want to delete files like the .git dir to bootstrap a new testsuite, correct? (Ctrl-C to abort, ENTER to continue)"
 
 find "${root_dirpath}" \
-    ! -name "${script_dirname}" \
+    ! -name bootstrap \
     ! -name example_impl \
     ! -name go.mod \
     ! -name go.sum \
