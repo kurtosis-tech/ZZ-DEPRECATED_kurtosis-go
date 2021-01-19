1c080815a8f90fdd15719411fcabe798fd6986c7
mieubrisse <mieubrisse@gmail.com>
2021-01-19 12:52:41 -0600

    Moving forward with building out the wrapper library's calls to lib core

--- lib/kurtosis/execution_config.go
+++ lib/kurtosis/execution_config.go
@@ -10,5 +10,5 @@ import "github.com/kurtosis-tech/kurtosis-go/lib/testsuite"
 type ExecutionConfig interface {
 	// Should take in the JSON of custom params intended for the testsuite, parse them, and create
 	//  an instance of the user's testsuite
-	CreateTestSuite(testSuiteParamsJson string) *testsuite.TestSuite
+	CreateTestSuite(testSuiteParamsJson string) (*testsuite.TestSuite, error)
 }
--- lib/kurtosis/kurtosis.go
+++ lib/kurtosis/kurtosis.go
@@ -6,8 +6,12 @@
 package kurtosis
 
 import (
+	"context"
+	"fmt"
+	"github.com/kurtosis-tech/kurtosis-go/lib_core/api/generated"
 	"github.com/kurtosis-tech/kurtosis-go/lib_core/lib_core_process_consts"
 	"github.com/palantir/stacktrace"
+	"google.golang.org/grpc"
 	"os"
 	"os/exec"
 	"strconv"
@@ -16,8 +20,7 @@ import (
 type executionAction int
 
 const (
-	printSuiteMetadataAction executionAction = iota
-	runTestAction
+	libCoreListenInterface = "127.0.0.1"
 
 	// TODO can make this configurable if it causes problems for the user's testsuite
 	libCoreListenPort = 5822
@@ -36,6 +39,16 @@ func (executor KurtosisExecutor) Execute(
 		libCoreParamsJson string,
 		suiteParamsJson string,
 		suiteLogLevel string) error {
+	ctx := context.Background()
+
+	testsuite, err := executor.executionConfig.CreateTestSuite(suiteParamsJson)
+	if err != nil {
+		return stacktrace.Propagate(
+			err,
+			"An error occurred instantiating the testsuite using custom params JSON '%v'",
+			suiteParamsJson)
+	}
+
 	// Launch the core lib as a separate process, communicable via gRPC
 	libCoreBinaryFilepath := executor.suiteContainerConfig.GetLibCoreBinaryFilepath()
 	libCoreLaunchingCmd := exec.Command(
@@ -50,13 +63,31 @@ func (executor KurtosisExecutor) Execute(
 		return stacktrace.Propagate(err, "An error occurred starting the lib core coprocess")
 	}
 
-	// TODO Create RPC client for sending messages to the coprocess
+	libCoreListenStr := fmt.Sprintf("%v:%v", libCoreListenInterface, libCoreListenPort)
+	conn, err := grpc.Dial(libCoreListenStr)
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred dialing the lib core process on '%v'", libCoreListenStr)
+	}
+	defer conn.Close()
 
-	// TODO actually call GetExecutionAction
-	executionAction := printSuiteMetadataAction
+	wrapperExecutionPathClient := generated.NewWrapperExecutionPathServiceClient(conn)
+	getPathResp, err := wrapperExecutionPathClient.GetPath(ctx, nil)
+	if err != nil {
+		return stacktrace.Propagate(err, "An error occurred making the call to lib core to get which execution path the wrapper lib should to go down")
+	}
+	path := getPathResp.GetPath()
 
-	if executionAction == printSuiteMetadataAction {
-
+	switch path {
+	case generated.WrapperExecutionPath_PRINT_SUITE_METADATA:
+		// TODO package up the test metadata
+		// TODO ask lib core to print the suite metadata
+	case generated.WrapperExecutionPath_RUN_TEST:
+		// TODO create a NetworkContext wrapper (which really just forwards all its calls to lib core)
+		// TODO get the test specified
+	default:
+		return stacktrace.NewError(
+			"Lib core responded that the wrapper should go down unknown execution path '%v'; this is a code bug",
+			path)
 	}
 
 	// TODO tell coprocess to kill itself
