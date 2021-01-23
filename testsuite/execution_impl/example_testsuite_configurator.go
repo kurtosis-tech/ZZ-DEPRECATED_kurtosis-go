df3b93c7dcc94a4efe0b447465079728fc852a12
mieubrisse <mieubrisse@gmail.com>
2021-01-22 23:04:08 -0600

    Add testsuite param validation

--- testsuite/execution_impl/example_testsuite_configurator.go
+++ testsuite/execution_impl/example_testsuite_configurator.go
@@ -11,6 +11,7 @@ import (
 	"github.com/kurtosis-tech/kurtosis-go/testsuite/testsuite_impl"
 	"github.com/palantir/stacktrace"
 	"github.com/sirupsen/logrus"
+	"strings"
 )
 
 type ExampleTestsuiteConfigurator struct {}
@@ -39,6 +40,20 @@ func (t ExampleTestsuiteConfigurator) ParseParamsAndCreateSuite(paramsJsonStr st
 		return nil, stacktrace.Propagate(err, "An error occurred deserializing the testsuite params JSON")
 	}
 
+	if err := validateArgs(args); args != nil {
+		return nil, stacktrace.Propagate(err, "An error occurred validating the deserialized testsuite params")
+	}
+
 	suite := testsuite_impl.NewExampleTestsuite(args.ApiServiceImage, args.DatastoreServiceImage, args.IsKurtosisCoreDevMode)
 	return suite, nil
+}
+
+func validateArgs(args ExampleTestsuiteArgs) error {
+	if strings.TrimSpace(args.ApiServiceImage) == "" {
+		return stacktrace.NewError("API service image is empty")
+	}
+	if strings.TrimSpace(args.DatastoreServiceImage) == "" {
+		return stacktrace.NewError("Datastore service image is empty")
+	}
+	return nil
 }
